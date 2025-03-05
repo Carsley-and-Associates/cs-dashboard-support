fd9e0b54591b2e592c640f23082c0d14b579928f
Yanmeeei <wyanmei@umich.edu>
2025-03-04 20:51:26 -0600

    fix: update export setting to include new function

--- CarsleyDashboardSupport/__init__.py
+++ CarsleyDashboardSupport/__init__.py
@@ -1,8 +1,7 @@
 from .status_types import StatusType, StatusEntry, Severity
 from .log_status import log_status
-from .sync_status import sync_status_now, start_sync_thread
+from .sync_status import sync_status_now, start_sync_thread, attempt_status_sync
 
-__all__ = ["StatusType", "StatusEntry", "Severity", "log_status", "sync_status_now", "start_sync_thread"]
-
-
-
+__all__ = ["StatusType", "StatusEntry", "Severity",
+           "log_status",
+           "sync_status_now", "start_sync_thread", "attempt_status_sync"]
